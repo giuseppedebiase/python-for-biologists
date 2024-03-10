9a5367ceb8f998141c3ff76eb02afb22c3063f9b
giuseppedebiase <133350197+giuseppedebiase@users.noreply.github.com>
2024-03-10 15:32:06 +0100

    Create 2.2_Complementing_DNA.py

---
+++ 2.2_Complementing_DNA.py
@@ -0,0 +1,12 @@
+# Here's a short DNA sequence:
+# ACTGATCGATTACGTATAGTATTTGCTATCATACATATATATCGATGCGTTCAT
+# Write a program that will print the complement of this sequence.
+
+my_dna = "ACTGATCGATTACGTATAGTATTTGCTATCATACATATATATCGATGCGTTCAT"
+my_dna_lower = my_dna.lower()
+#replace is case sensitive, so I used the lower method to convert uppercase letters to lower case and then I used replace to replace lower case bases to their complementary (in uppercase)
+my_dna_a_T = my_dna_lower.replace("a", "T")
+my_dna_t_A = my_dna_a_T.replace("t", "A")
+my_dna_c_G = my_dna_t_A.replace("c", "G")
+my_dna_g_C = my_dna_c_G.replace("g", "C")
+print("The complemntary sequence of my_dna is: " + my_dna_g_C)
