90b27022393a9bddf5c7229ec58ca6dddb67ba79
giuseppedebiase <133350197+giuseppedebiase@users.noreply.github.com>
2024-03-14 12:14:21 +0100

    Add files via upload

---
+++ 3.3_Writing_multiple_FASTA_files.py
@@ -0,0 +1,15 @@
+# Use the data from the previous exercise, but instead of creating a single FASTA file, 
+# create three new FASTA files – one per sequence. The names of the FASTA files 
+# should be the same as the sequence header names, with the extension .fasta.
+
+sequence_one = open("ABC123.fasta", "w")
+sequence_one.write(">ABC123\nATCGTACGATCGATCGATCGCTAGACGTATCG")
+sequence_one.close()
+
+sequence_two = open("DEF456.fasta", "w")
+sequence_two.write(">DEF456\nactgatcgacgatcgatcgatcacgact")
+sequence_two.close()
+
+sequence_three = open("HIJ789.fasta", "w")
+sequence_three.write(">HIJ789\nACTGAC-ACTGT--ACTGTA----CATGTG")
+sequence_three.close()
