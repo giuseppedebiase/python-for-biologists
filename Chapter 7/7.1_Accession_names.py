960942f31dd136006e7dae98ce338ea4dd5a4832
giuseppedebiase <133350197+giuseppedebiase@users.noreply.github.com>
2024-03-22 18:37:23 +0100

    Update 7.1_Accession_names.py

--- Chapter 7/7.1_Accession_names.py
+++ Chapter 7/7.1_Accession_names.py
@@ -47,6 +47,13 @@ for accession in accession_list:
         contain_d_anything_e.append(accession)
 
     #contain both the letters d and e in any order
+    #in theory this should add to the list only the accession names that contain the following pattern: d[any n.° of letters]e or e[any n.° of letters]d
+    #in the script given by the author as a solution the if statement for this part is the following:
+    #if re.search(r"d.*e", acc) or re.search(r"e.*d", acc):
+    #which matches "d[any n.° of letters]e or e[any n.° of letters]d" concept
+    #and gives as output the following ac names when run: eihd39d9, chdsye847, hdle3455, xjhd53e, de37dp
+    #hwoever in the book it's written that the acc names that should be given as the output should be: hedle3455, de37dp
+    #so the acc names shouldn't contain the patterns "d[any n.° of letters]e" or "e[any n.° of letters]d" but just "de" or "ed"
     if re.search(r"de|ed", accession):
         contain_d_e_any_order.append(accession)
     
@@ -59,6 +66,8 @@ for accession in accession_list:
         start_x_or_y_end_e.append(accession)
 
     #contain three of more numbers in a row
+    #this if works because acc names with 4 (or more) numbers are basically acc names 
+    #that contain 3 numbers followed by 1 (or more) numbers
     if re.search(r"[0123456789]{3}", accession):
         contain_minimum_three_numbers_in_a_row.append(accession)
     
