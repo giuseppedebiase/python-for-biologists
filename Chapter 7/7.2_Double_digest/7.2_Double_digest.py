c8e6b15a06285f443b1718764c746204d96f91b8
giuseppedebiase <133350197+giuseppedebiase@users.noreply.github.com>
2024-04-01 18:25:54 +0200

    Update 7.2_Double_digest.py

--- Chapter 7/7.2_Double_digest/7.2_Double_digest.py
+++ Chapter 7/7.2_Double_digest/7.2_Double_digest.py
@@ -1,10 +1,11 @@
-# In the chapter_7 file inside the exercises download, there's a file called dna.txt
-# which contains a made-up DNA sequence. Predict the fragment lengths that we will
-# get if we digest the sequence with two made-up restriction enzymes – AbcI, whose 
-# recognition site is ANT*AAT, and AbcII, whose recognition site is GCRW*TG 
-# (asterisks indicate the position of the cut site).
+'''In the chapter_7 file inside the exercises download, there's a file called dna.txt
+which contains a made-up DNA sequence. Predict the fragment lengths that we will
+get if we digest the sequence with two made-up restriction enzymes – AbcI, whose 
+recognition site is ANT*AAT, and AbcII, whose recognition site is GCRW*TG 
+(asterisks indicate the position of the cut site).
 
-#N means any nucleotide, R is A or G, W is A or T
+N means any nucleotide, R is A or G, W is A or T
+'''
 
 import re
 file = open("dna.txt")
