a8be7bedc2579b70239a2a84702e7da5e7058c68
giuseppedebiase <133350197+giuseppedebiase@users.noreply.github.com>
2024-04-01 18:24:48 +0200

    Update 6.5_High_low_medium.py

--- Chapter 6/6.5_High_low_medium/6.5_High_low_medium.py
+++ Chapter 6/6.5_High_low_medium/6.5_High_low_medium.py
@@ -1,14 +1,16 @@
-# In the chapter_6 folder in the exercises download, you'll find a text file called 
-# data.csv, containing some made-up data for a number of genes. Each line contains 
-# the following fields for a single gene in this order: species name, sequence, gene 
-# name, expression level. The fields are separated by commas (hence the name of the 
-# file – csv stands for Comma Separated Values). Think of it as a representation of a 
-# table in a spreadsheet – each line is a row, and each field in a line is a column. All 
-# the exercises for this chapter use the data read from this file. 
+'''
+In the chapter_6 folder in the exercises download, you'll find a text file called 
+data.csv, containing some made-up data for a number of genes. Each line contains 
+the following fields for a single gene in this order: species name, sequence, gene 
+name, expression level. The fields are separated by commas (hence the name of the 
+file – csv stands for Comma Separated Values). Think of it as a representation of a 
+table in a spreadsheet – each line is a row, and each field in a line is a column. All 
+the exercises for this chapter use the data read from this file. 
 
-# For each gene, print out a message giving the gene name and saying whether its AT 
-# content is high (greater than 0.65), low (less than 0.45) or medium (between 0.45 
-# and 0.65).
+For each gene, print out a message giving the gene name and saying whether its AT 
+content is high (greater than 0.65), low (less than 0.45) or medium (between 0.45 
+and 0.65).
+'''
 
 file = open("data.csv")
 
