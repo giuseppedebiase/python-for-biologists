92e008c41e4895df5c01a8b840908921bab5300c
giuseppedebiase <133350197+giuseppedebiase@users.noreply.github.com>
2024-04-01 18:22:37 +0200

    Update 6.1_Several_species.py

--- Chapter 6/6.1_Several_species/6.1_Several_species.py
+++ Chapter 6/6.1_Several_species/6.1_Several_species.py
@@ -1,13 +1,15 @@
-# In the chapter_6 folder in the exercises download, you'll find a text file called 
-# data.csv, containing some made-up data for a number of genes. Each line contains 
-# the following fields for a single gene in this order: species name, sequence, gene 
-# name, expression level. The fields are separated by commas (hence the name of the 
-# file – csv stands for Comma Separated Values). Think of it as a representation of a 
-# table in a spreadsheet – each line is a row, and each field in a line is a column. All 
-# the exercises for this chapter use the data read from this file. 
+'''
+In the chapter_6 folder in the exercises download, you'll find a text file called 
+data.csv, containing some made-up data for a number of genes. Each line contains 
+the following fields for a single gene in this order: species name, sequence, gene 
+name, expression level. The fields are separated by commas (hence the name of the 
+file – csv stands for Comma Separated Values). Think of it as a representation of a 
+table in a spreadsheet – each line is a row, and each field in a line is a column. All 
+the exercises for this chapter use the data read from this file. 
 
-# Print out the gene names for all genes belonging to Drosophila melanogaster or 
-# Drosophila simulans. 
+Print out the gene names for all genes belonging to Drosophila melanogaster or 
+Drosophila simulans. 
+'''
 
 file = open("data.csv")
 
