ad949299dd3bcdede948bf721431d671ead7d84f
giuseppedebiase <133350197+giuseppedebiase@users.noreply.github.com>
2024-04-01 18:15:41 +0200

    Update 2.1_Calculating_AT_content.py

--- Chapter 2/2.1_Calculating_AT_content.py
+++ Chapter 2/2.1_Calculating_AT_content.py
@@ -1,9 +1,10 @@
-# Here's a short DNA sequence:
-# ACTGATCGATTACGTATAGTATTTGCTATCATACATATATATCGATGCGTTCAT
-# Write a program that will print out the AT content of this DNA sequence. Hint: you
-# can use normal mathematical symbols like add (+), subtract (-), multiply (*), divide 
-# (/) and parentheses to carry out calculations on numbers in Python. 
-
+'''
+Here's a short DNA sequence:
+ACTGATCGATTACGTATAGTATTTGCTATCATACATATATATCGATGCGTTCAT
+Write a program that will print out the AT content of this DNA sequence. Hint: you
+can use normal mathematical symbols like add (+), subtract (-), multiply (*), divide 
+(/) and parentheses to carry out calculations on numbers in Python. 
+'''
 my_dna = "ACTGATCGATTACGTATAGTATTTGCTATCATACATATATATCGATGCGTTCAT"
 AT_count = my_dna.count("A") + my_dna.count("T")
 #The formula for calculating AT content is n° of A+T/length of the sequence
