dcf58bd58e127696a759041c7752e34837994b8e
giuseppedebiase <133350197+giuseppedebiase@users.noreply.github.com>
2024-04-01 18:25:29 +0200

    Update 7.1_Accession_names.py

--- Chapter 7/7.1_Accession_names.py
+++ Chapter 7/7.1_Accession_names.py
@@ -1,16 +1,18 @@
-# Here's a list of made-up gene accession names:
-# xkn59438, yhdck2, eihd39d9, chdsye847, hedle3455, xjhd53e, 45da, de37dp
-# Write a program that will print only the accession names that satisfy the following 
-# criteria – treat each criterion separately:
-# • contain the number 5
-# • contain the letter d or e
-# • contain the letters d and e in that order
-# • contain the letters d and e in that order with a single letter between them
-# • contain both the letters d and e in any order
-# • start with x or y
-# • start with x or y and end with e
-# • contain three or more numbers in a row
-# • end with d followed by either a, r or p
+'''
+Here's a list of made-up gene accession names:
+xkn59438, yhdck2, eihd39d9, chdsye847, hedle3455, xjhd53e, 45da, de37dp
+Write a program that will print only the accession names that satisfy the following 
+criteria – treat each criterion separately:
+• contain the number 5
+• contain the letter d or e
+• contain the letters d and e in that order
+• contain the letters d and e in that order with a single letter between them
+• contain both the letters d and e in any order
+• start with x or y
+• start with x or y and end with e
+• contain three or more numbers in a row
+• end with d followed by either a, r or p
+'''
 
 import re
 
